efc9cae11fad6dac69e5eb652b8296afe7d0519f
milicajankocivfon <jankocivjelenamejl@gmail.com>
2026-03-09 20:07:34 +0100

    hosts

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        allowedHosts: ['reliable-education-production-3683.up.railway.app']
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
