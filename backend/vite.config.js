0521db89bbf0c55bf5443101f524fc789451cdaa
milicajankocivfon <jankocivjelenamejl@gmail.com>
2026-03-09 20:18:35 +0100

    hosts-ispravka

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-
-export default defineConfig({
-    server: {
-        allowedHosts: ['reliable-education-production-3683.up.railway.app']
-    },
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-});
+import { defineConfig } from 'vite'; 
+import laravel from 'laravel-vite-plugin'; 
+export default defineConfig(
+    { plugins: [ laravel({ 
+        input: ['resources/css/app.css', 'resources/js/app.js'],
+         refresh: true, }), ], }
+    );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+        allowedHosts: ['reliable-education-production-3683.up.railway.app']
+    },
 })
